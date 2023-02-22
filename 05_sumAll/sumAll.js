c8b22527f805bed68643db691a604006373e0506
Thomas McCann <webdev@millom.net>
2023-02-22 12:44:46 +0000

    Comppleted excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  //checks if int1 and int2 are numbers
+  if (typeof int1 !== 'number' || typeof int2 !== 'number') {
+    return 'ERROR';
+  } else if (int1 < 0 || int2 < 0) {
+    return 'ERROR';
+  } else if (int1 > int2) {
+    let sum = 0;
+    for (let i = int2; i <= int1; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    let sum = 0;
+    for (let i = int1; i <= int2; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
