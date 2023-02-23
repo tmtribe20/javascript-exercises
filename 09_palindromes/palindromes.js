877f66928dc0407dd87e1f476d32b61df78bb189
Thomas McCann <webdev@millom.net>
2023-02-23 22:20:32 +0000

    Excercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const strArr = str.toLowerCase().split(''); // convert to lowercase and split into array
+  const strArrFiltered = strArr.filter((char) => {
+    return char.match(/[a-z]/); // filter out non-alphabetic characters
+  });
+  const strArrReversed = strArrFiltered.slice().reverse(); // copy array and reverse it
+  return strArrFiltered.join('') === strArrReversed.join(''); // compare the two arrays
 };
 
 // Do not edit below this line
