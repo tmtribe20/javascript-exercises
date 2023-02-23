dd8af615e6fc5e64d3bf51a6cfa9420939126186
Thomas McCann <webdev@millom.net>
2023-02-23 22:48:51 +0000

    Completed excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n === 'string') {
+    //if number is a string convert to number
+    n = Number(n);
+  }
 
+  if (n < 0) return 'OOPS'; // if n is negative, return 'OOPS'
+  if (n === 1) return 1; // if n is 1, return 1
+  if (n === 2) return 1; // if n is 2, return 1
+  //if string convert to number
+  if (typeof n === 'string') {
+    n = Number(n);
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2); // else, return the sum of the previous two numbers
 };
 
 // Do not edit below this line
--- 10_fibonacci/solution/fibonacci-solution.js
+++ 10_fibonacci/solution/fibonacci-solution.js
@@ -1,9 +1,10 @@
 const fibonacci = function (count) {
-  if (count < 0) return "OOPS";
-  if (count === 0) return 0;
-  let a = 0;
-  let b = 1;
+  if (count < 0) return 'OOPS'; // if n is negative, return 'OOPS'
+  if (count === 0) return 0; // if n is 0, return 0
+  let a = 0; // if n is 1, return 1
+  let b = 1; // if n is 2, return 1
   for (let i = 1; i < count; i++) {
+    //
     const temp = b;
     b = a + b;
     a = temp;
